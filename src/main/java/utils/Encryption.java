457ce3690c02a9c684136bdfc902eb377889f72a
Morten Danielsen <Morten@Mortens-MacBook-Pro.local>
2018-11-29 15:09:12 +0100

    XOR explained

--- src/main/java/utils/Encryption.java
+++ src/main/java/utils/Encryption.java
@@ -17,6 +17,9 @@ public final class Encryption {
       StringBuilder thisIsEncrypted = new StringBuilder();
 
       // TODO: This is where the magic of XOR is happening. Are you able to explain what is going on?
+      /* For every value that is going to be encrypted, this for-loop iterates through what is typed in, and adds
+      the binary value of my encryption_key which is declared as "key" in linje 14. To this we use the class
+      StringBuilder, which the declared in line 17 to be the instance "thisIsEncrypted". */
       for (int i = 0; i < rawString.length(); i++) {
         thisIsEncrypted.append((char) (rawString.charAt(i) ^ key[i % key.length]));
       }
